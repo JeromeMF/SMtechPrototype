d8e5c3376eef5c7fae3c734180cc37b5e4c8b111
Jerome Figueiredo <jerome.figueiredo@puregym.com>
2023-01-27 11:17:17 +0000

    Initial Commit

---
+++ SMTechPrototype/SMTechPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMTechPrototypeApp.swift
+//  SMTechPrototype
+//
+//  Created by Jérôme Figueiredo on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SMTechPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
